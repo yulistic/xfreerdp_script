c74bde07a3d1fd3706fe7167dfc81368266d5914
Jongyul Kim <yulistic@gmail.com>
2017-04-04 11:10:36 +0900

    Initial commit

---
+++ rdp.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+xfreerdp /multimon /u:yulistic /v:143.248.188.251 /f /p:Whddbf17o /bpp:32 +fonts /jpeg-quality:100 +themes +wallpaper /gfx /rfx /drive:USB,/run/media/yulistic /compression
+#xfreerdp /u:yulistic /v:143.248.188.251 /f /p:Whddbf17o /bpp:32 +fonts /jpeg-quality:100 +themes +wallpaper /rfx /drive:USB,/run/media/yulistic
